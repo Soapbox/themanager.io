2e285914a3355190688f89a0cf0d8333a297c876
Julian <julian@soapboxhq.com>
2019-08-16 10:35:24 -0400

    added GTM scripts

--- header.php
+++ header.php
@@ -18,9 +18,22 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
   <link href="./gulp/js/vendor/all.js" rel="stylesheet">
 	<?php wp_head(); ?>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-M5ZGD2Q');</script>
+  <!-- End Google Tag Manager -->
 </head>
 
 <body <?php body_class(); ?>>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5ZGD2Q"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <div id="page" class="site">
   <header id="masthead" class="site-header">
     <div class="grid-container flex-container align-justify align-middle">
